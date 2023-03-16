6935079aa7478a087e4471ecf62eb7f5ce06a460
Carlos Henrique <dh.henrique1@hotmail.com>
2023-03-16 20:53:01 -0300

    Adicionando exercicio 01 - lista de pares e impares com média geral.

---
+++ Ex_01.py
@@ -0,0 +1,23 @@
+#Exercício 01 - Solicitar a inserção de 5 números, ao final, imprimir os números pares, números ímpares e a média geral
+# desses números.
+
+lista = []
+lista_pares = []
+lista_impares = []
+
+print('Digite 5 números!')
+
+for n in range(5):
+    lista.append(int(input('Numero: ')))
+
+for n in lista:
+    if n % 2 == 0:
+        lista_pares.append(n)
+    else:
+        lista_impares.append(n)
+
+media = sum(lista) / len(lista)
+
+print(f'Média geral: {media:.2f}')
+print(f'Os numeros pares são: {lista_pares}')
+print(f'Os numeros impares são: {lista_impares}')
